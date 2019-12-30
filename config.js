9c0487769baf6a02da69cfc67d9dcfc52b14f80c
BlackHawk94 <ahah90909090@gmail.com>
2019-12-30 15:36:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     username: "Sakon90HD",
     token: "oauth:dbu8rb0f7bgh778i3vhgeixkuthvgj",
     channels: [
-        "<pepo3393live>",
-        "<bnicot>",
-        "<celletto91>"
+        "Pepo3393Live",
+        "Bnicot",
+        "celletto91"
     ]
 }
