1c6af204c0513a25511d56ff6d94a8ce9874eea5
BlackHawk94 <ahah90909090@gmail.com>
2019-12-30 16:17:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
 module.exports = {
     username: "sakon90hd",
     token: "oauth:dbu8rb0f7bgh778i3vhgeixkuthvgj",
-    channels: "Pepo3393Live"
+    channels: [
+        "pepo3393live",
+        "bnicot",
+        "celletto91",
+        "copykat_",
+        "limitlessh",
+        "lady_ally",
+        "gracesvringhi",
+        "the_foox",
+        "genesis_channel",
+        "giuseppe88"
+    ]
 }
